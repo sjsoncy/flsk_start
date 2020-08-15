213fe38ce8db46dd41c0b274ca55e58a5e3b46f5
sjsoncy <sjsoncy@naver.com>
2020-08-15 16:50:37 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+# 서버 앱 생성
+app = Flask(__name__)
+
+#url 세팅 - 라우터 설정
+dapp.route('/')
+def index():
+    # return "<h1>인덱스 페이지</h1>"
+    return render_template("index.html")
+
+# 앱실행
+if __name__ == "__main__":
+    app.run(debug=True) #가동
+
